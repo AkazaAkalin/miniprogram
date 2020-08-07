fff732aa8609e8db53f6ce735c0df79fbac01df6
Guohua <873889509@qq.com>
2020-08-07 15:36:27 +0800

    'commit'

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const images = require('../../utils/image.js')
+const tabbar = require('../../components/tabbar/tabbar.js')
+Page({
+  data: {
+    indexImg: images.images.index.indexImg
+  },
+  //事件处理函数
+  onLoad: function () {
+    tabbar.tabbar("tabBar", 0, this)
+  },
+  navi(e) {
+    tabbar.navi(e.currentTarget.dataset.url)
+  },
+})
